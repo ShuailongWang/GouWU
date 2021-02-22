7b9f0fbad9b82f50fe3536a278c96bb63d3a175f
ShuailongWang <1250626642@qq.com>
2021-02-22 23:56:52 +0800

    baseUrl

--- GouWu/request/index.js
+++ GouWu/request/index.js
@@ -1,5 +1,6 @@
 
 export const request = (params) => {
+  const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1"
   return new Promise((resolve, reject) => {
     wx.request({
       ...params,
